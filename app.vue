c8d5944ddc525c546868e8511f0045bfa1dbafb0
SergioIgnatev <ignatevsergey17@gmail.com>
2024-06-25 16:18:44 +0300

    a mistake

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
 <h1>H!</h1>
+<p v-for="m in mappedItems">{{ m.text }}</p>
   </div>
 </template>
+<script setup>
+const mappedItems=ref([
+  {id:1,text:"HERE"},
+  {id:2,text:"There"}
+])
+</script>
